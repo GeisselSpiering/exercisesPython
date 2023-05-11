fbd20cef749ccd3e671ba846f6c74b127bec9496
Geissel Spiering <88937387+GeisselSpiering@users.noreply.github.com>
2023-05-10 22:09:24 -0300

    Create impressaoNumeros.py
    Desenvolva um programa que imprima os números 1, 10, 100 e 1000 utilizando um espaço de 7 colunas, alinhando o conteúdo à esquerda, depois centralizando e em seguida alinhando à direita.

---
+++ impressaoNumeros.py
@@ -0,0 +1,8 @@
+number_01 = 1
+number_02 = 10
+number_03 = 100
+number_04 = 1000
+
+print(f"{number_01:_<7d}\n{number_02:_<7d}\n{number_03:_<7d}\n{number_04:_<7d}\n")
+print(f"{number_01:_^7d}\n{number_02:_^7d}\n{number_03:_^7d}\n{number_04:_^7d}\n")
+print(f"{number_01:_>7d}\n{number_02:_>7d}\n{number_03:_>7d}\n{number_04:_>7d}\n")
